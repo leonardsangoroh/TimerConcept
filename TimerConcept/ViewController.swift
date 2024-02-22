f60d148d41d4c952c4933a5ab177d917e56be1ac
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-22 13:42:56 +0300

    feat: invalidate timer

--- TimerConcept/ViewController.swift
+++ TimerConcept/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     let colors: [UIColor] = [.systemRed, .systemBlue, .systemCyan, .systemMint, .systemPink, .systemTeal]
+    var timer:Timer = Timer.init()
+    var runTimerLimit:Float = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +23,8 @@ class ViewController: UIViewController {
     }
 
     func createTimer(){
-        let timer = Timer.scheduledTimer(timeInterval: 2,
+        
+        timer = Timer.scheduledTimer(timeInterval: 2,
                                          target: self,
                                          selector: #selector(fireTimer),
                                          userInfo: nil,
@@ -29,10 +32,21 @@ class ViewController: UIViewController {
         /// in miliseconds
         /// gives system time flexibility to execute tasks
         timer.tolerance = 200
+        
+        //stop timer
+        //timer.invalidate()
+
     }
     
     @objc func fireTimer(){
         view.backgroundColor = colors.randomElement()
+        runTimerLimit += 1
+        print(runTimerLimit)
+        
+        if runTimerLimit >= 10.0{
+            timer.invalidate()
+        }
     }
+    
 }
 
