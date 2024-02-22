af8836f979d953be99676afcb34b81dfbc4a152a
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-22 12:17:49 +0300

    chore: initialize TimerConcept application

---
+++ TimerConcept/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerConcept
+//
+//  Created by Lee Sangoroh on 22/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
