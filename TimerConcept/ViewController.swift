cdd3eabb8c6177f55757517beb5af790c32beeb3
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-22 12:52:30 +0300

    feat: add a color introductory Timer

--- TimerConcept/ViewController.swift
+++ TimerConcept/ViewController.swift
@@ -13,7 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        createTimer()
+    }
 
-
+    func createTimer(){
+        let timer = Timer.scheduledTimer(timeInterval: 2,
+                                         target: self,
+                                         selector: #selector(fireTimer),
+                                         userInfo: nil,
+                                         repeats: false)
+        DispatchQueue.main.asyncAfter(deadline: .now()+3, execute: {
+            timer.fire()
+        })
+    }
+    
+    @objc func fireTimer(){
+        view.backgroundColor = .systemRed
+    }
 }
 
